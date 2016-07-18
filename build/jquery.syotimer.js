9e42a7dd3970f9f34ad9fdccb8e2f3ebbbf43c68
MacJohn <donald28@mail.ru>
2016-07-18 23:21:05 +0300

    setting build, continue fix plugin code #1

--- build/jquery.syotimer.js
+++ build/jquery.syotimer.js
@@ -1,62 +1,30 @@
 /**
- * SyoTimer 
+ * SyoTimer - countdown jquery plugin
  * @version: 1.1.0 
  * @author: John Syomochkin 
  * @homepage: https://github.com/mrfratello/SyoTimer#readme 
- * 
- * The MIT License (MIT)
+ * @date: 2016.7.18
+ * @license under MIT license
  */
-/**
- * jquery.syotimer.js
- * @version: v1.0.1
- * @author: John Syomochkin
- * @url: https://github.com/mrfratello/SyoTimer
- *
- * Created by John Syomochkin on 2014-12-10.
- *
- *
- * The MIT License (MIT)
- *
- * Copyright (c) 2014 John Syomochkin
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 (function($){
-    var day_ms = 24 * 60 * 60,
-        hour_ms = 60 * 60,
-        minute_ms = 60,
+    const DAY_MS = 24 * 60 * 60;
+    const HOUR_MS = 60 * 60;
+    const MINUTE_MS = 60;
 
-        lang = {
-            rus: {
-                second: ['секунда', 'секунды', 'секунд'],
-                minute: ['минута', 'минуты', 'минут'],
-                hour: ['час', 'часа', 'часов'],
-                day: ['день', 'дня', 'дней']
-            },
-            eng: {
-                second: ['second', 'seconds'],
-                minute: ['minute', 'minutes'],
-                hour: ['hour', 'hours'],
-                day: ['day', 'days']
-            }
-        };
+    var lang = {
+        rus: {
+            second: ['секунда', 'секунды', 'секунд'],
+            minute: ['минута', 'минуты', 'минут'],
+            hour: ['час', 'часа', 'часов'],
+            day: ['день', 'дня', 'дней']
+        },
+        eng: {
+            second: ['second', 'seconds'],
+            minute: ['minute', 'minutes'],
+            hour: ['hour', 'hours'],
+            day: ['day', 'days']
+        }
+    };
     $.fn.syotimer = function(options){
         // установки по умолчанию
         var defaultTimer = {
@@ -94,51 +62,30 @@
             obj = init( obj, settings );
 
             (function timeout(){
-                $('.second .tab-val',obj).css('opacity',1);
-                Now = new Date(); // текущая дата
-                DeadDate = new Date(settings.year, settings.month-1, settings.day, settings.hour, settings.minute, settings.second); // контрольная дата
+                $('.second .tab-val', obj).css( 'opacity', 1 );
+                var currentDate = new Date(),
+                    deadLineDate = new Date(
+                        settings.year,
+                        settings.month - 1,
+                        settings.day,
+                        settings.hour,
+                        settings.minute,
+                        settings.second
+                    );
 
-                different = Math.floor((DeadDate.getTime()-Now.getTime())/1000);
-                if ( settings.periodic ){ // если надо отсчитывать таймер по периоду
-
-                    // определяем единицу измерения периода таймера
-                    switch (settings.periodUnit){
-                        case 'd': unit_ms = day_ms; break;
-                        case 'h': unit_ms = hour_ms; break;
-                        case 'm': unit_ms = minute_ms; break;
-                        case 's': unit_ms = 1; break;
-                    }
-
-                    differentUnits = Math.abs( Math.ceil( (DeadDate.getTime()-Now.getTime())/(unit_ms*1000) ) ); // кол-во полных единиц времени между текущей датой и дедлайном
-
-                    if ( different>=0 ){
-                        dUnits = differentUnits%settings.periodInterval;
-                        dUnits = (dUnits==0)? settings.periodInterval-1 : dUnits-1;
-                    } else
-                        dUnits = settings.periodInterval-differentUnits%settings.periodInterval;
-
-                    addUnits = different%unit_ms;
-
-                    // fix когда дедлайн раньше текущей даты, возникает баг с неправильным расчетом интервала при different пропорциональной periodUnit
-                    if ( ( addUnits == 0 ) && ( different < 0 ) )
-                        dUnits--;
-
-                    alls = Math.abs( dUnits * unit_ms + addUnits );
-
-                } else
-                    alls = different;
+                var alls = getSecondsCountToDeadLine(currentDate, deadLineDate, settings);
 
                 // количество секунд до дедлайна (alls) при наличии
                 if ( alls >= 0 ){
-                    dd = Math.floor(alls/day_ms);
+                    dd = Math.floor(alls/DAY_MS);
 
-                    alls = alls%day_ms;
-                    dh = Math.floor(alls/hour_ms);
+                    alls = alls%DAY_MS;
+                    dh = Math.floor(alls/HOUR_MS);
 
-                    alls = alls%hour_ms;
-                    dm = Math.floor(alls/minute_ms);
+                    alls = alls%HOUR_MS;
+                    dm = Math.floor(alls/MINUTE_MS);
 
-                    alls = alls%minute_ms;
+                    alls = alls%MINUTE_MS;
                     ds = Math.floor(alls);
 
                     if ( settings.dayVisible ){
@@ -148,7 +95,7 @@
                         $('.hour .tab-val',obj).html(format2(dh, settings.dubleNumbers));
                         $('.hour .tab-metr',obj).html(definitionOfNumerals(dh, lang[settings.lang].hour, settings.lang));
                     } else {
-                        dh+=dd*24;
+                        dh += dd * 24;
                         $('.hour .tab-val',obj).html(format2(dh, settings.dubleNumbers));
                         $('.hour .tab-metr',obj).html(definitionOfNumerals(dh, lang[settings.lang].hour, settings.lang));
                     }
@@ -176,6 +123,60 @@
         });
     };
 
+    /**
+     *
+     * @param currentDate
+     * @param deadLineDate
+     * @param settings
+     * @returns {*}
+     */
+    function getSecondsCountToDeadLine(currentDate, deadLineDate, settings) {
+        var alls,
+            differenceInSeconds = (deadLineDate.getTime() - currentDate.getTime()) / 1000;
+        differenceInSeconds = Math.floor( differenceInSeconds );
+        if ( settings.periodic ) { // если надо отсчитывать таймер по периоду
+            var periodUnitInMilliSec = getPeriodUnit(settings.periodUnit),
+                fullTimeUnitsBetween = (deadLineDate.getTime() - currentDate.getTime()) / (periodUnitInMilliSec * 1000);
+            fullTimeUnitsBetween = Math.ceil( fullTimeUnitsBetween );
+            fullTimeUnitsBetween = Math.abs( fullTimeUnitsBetween );
+            if ( differenceInSeconds >= 0 ) {
+                dUnits = fullTimeUnitsBetween%settings.periodInterval;
+                dUnits = (dUnits==0)? settings.periodInterval-1 : dUnits-1;
+            } else {
+                dUnits = settings.periodInterval - fullTimeUnitsBetween % settings.periodInterval;
+            }
+            addUnits = differenceInSeconds % periodUnitInMilliSec;
+
+            // fix когда дедлайн раньше текущей даты, возникает баг с неправильным расчетом интервала при different пропорциональной periodUnit
+            if ( ( addUnits == 0 ) && ( differenceInSeconds < 0 ) )
+                dUnits--;
+
+            alls = Math.abs( dUnits * periodUnitInMilliSec + addUnits );
+
+        } else
+            alls = differenceInSeconds;
+        return alls;
+    }
+
+    /**
+     * Determine a unit of period in milliseconds
+     * @returns {number}
+     */
+    function getPeriodUnit(given_period_unit) {
+        switch (given_period_unit) {
+            case 'd': return DAY_MS;
+            case 'h': return HOUR_MS;
+            case 'm': return MINUTE_MS;
+            case 's': return 1;
+        }
+    }
+
+    /**
+     *
+     * @param elem
+     * @param options
+     * @returns {*|Object|void}
+     */
     function init( elem, options ) { // установка html разметки в блоке с таймером
         var timerDom,
             dayCellDom = ( options.dayVisible) ? getCellDom('day', '0') : '';
@@ -199,6 +200,13 @@
             };
         return $.extend(elem, timerBlocks);
     }
+
+    /**
+     * Return once cell DOM of countdown: day, hour, minute, second
+     * @param cls                 class of cell
+     * @param start_count_format
+     * @returns {string}
+     */
     function getCellDom(cls, start_count_format) {
         cls = cls || '';
         start_count_format = start_count_format || '00';
@@ -208,10 +216,25 @@
                 '<div class="tab-metr"></div>' +
             '</div>';
     }
+
+    /**
+     *
+     * @param ANumber
+     * @param isUse
+     * @returns {string}
+     */
     function format2(ANumber, isUse){ // формирования чисел с ведущими нулями
         isUse = (isUse !== false) ? true : false;
         return ( ( ANumber <= 9 ) && isUse ) ? ( "0" + ANumber ) : ( "" + ANumber );
     }
+
+    /**
+     *
+     * @param number
+     * @param titles
+     * @param lang
+     * @returns {*}
+     */
     function definitionOfNumerals(number, titles, lang){ // установка правильного склонения после числительных
         switch (lang){
             case 'rus':
--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,25 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var header = require('gulp-header');
 var rename = require('gulp-rename');
-//var pump = require('pump');
 var pkg = require('./package.json');
+
+function getToday() {
+    var today = new Date();
+    return [
+        today.getFullYear(),
+        today.getMonth() + 1,
+        today.getDate()
+    ].join('.');
+}
 
 gulp.task('build', function() {
     var banner = ["/**",
-        " * <%= pkg.projectName %> ",
+        " * <%= pkg.projectName %> - <%= pkg.description %>",
         " * @version: <%= pkg.version %> ",
         " * @author: <%= pkg.author %> ",
         " * @homepage: <%= pkg.homepage %> ",
-        " * ",
-        " * The MIT License (MIT)",
+        " * @date: " + getToday(),
+        " * @license: under MIT license",
         " */",
     ""].join("\n");
     return gulp.src('source/*.js')
@@ -21,7 +29,7 @@ gulp.task('build', function() {
 });
 
 gulp.task('compress', function() {
-    var banner = "/** <%= pkg.projectName %> v.<%= pkg.version %> | MIT licence */\n";
+    var banner = "/** <%= pkg.projectName %> v.<%= pkg.version %> | under MIT licence */\n";
     return gulp.src('source/*.js')
         .pipe(uglify())
         .pipe(header(banner, { pkg : pkg } ))
